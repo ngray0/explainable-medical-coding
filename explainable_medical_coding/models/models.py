6b685e8868e690d722146211cd14292c9433b83c
grayn0 <nickygray45@gmail.com>
2025-07-18 18:45:54 +0100

    selective grad checkpointing

--- explainable_medical_coding/models/models.py
+++ explainable_medical_coding/models/models.py
@@ -91,20 +91,21 @@ class PLMICD(nn.Module):
             )
 
     def _enable_selective_gradient_checkpointing(self):
-        """Enable gradient checkpointing for only the second half of transformer layers."""
+        """Enable gradient checkpointing for 70% of transformer layers."""
         # Get total number of layers
         total_layers = len(self.roberta_encoder.encoder.layer)
-        checkpoint_start = total_layers // 2
+        num_checkpoint_layers = int(total_layers * 0.7)
+        checkpoint_start = total_layers - num_checkpoint_layers
         
         # Disable global checkpointing first
         self.roberta_encoder.gradient_checkpointing = False
         
-        # Enable checkpointing only for second half of layers
+        # Enable checkpointing for last 70% of layers
         for i in range(checkpoint_start, total_layers):
             layer = self.roberta_encoder.encoder.layer[i]
             layer.gradient_checkpointing = True
             
-        print(f"Enabled gradient checkpointing for layers {checkpoint_start}-{total_layers-1} of {total_layers}")
+        print(f"Enabled gradient checkpointing for {num_checkpoint_layers} layers ({checkpoint_start}-{total_layers-1}) of {total_layers}")
 
     @torch.no_grad()
     def predict(
